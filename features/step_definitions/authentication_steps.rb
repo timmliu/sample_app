a1f4f76ebb61ea2fe2579d9382fa63584fcf3a13
timm.liu <timm.liu@activenetwork.com>
2013-11-12 14:32:40 -0800

    finish sign in stuff ch8

---
+++ features/step_definitions/authentication_steps.rb
@@ -0,0 +1,32 @@
+# Unsuccessful signin
+Given /^a user visits the signin page$/ do
+  visit signin_path
+end
+
+When /^she submits invalid signin information$/ do
+  click_button "Sign in"
+end
+
+Then /^she should see an error message$/ do
+  expect(page).to have_selector('div.alert.alert-error')
+end
+
+# Successful signin
+And /^the user has an account$/ do
+  @user = User.create(name: "Example User", email: "user@example.com",
+  				password: "foobar", password_confirmation: "foobar")
+end
+
+When /^the user submits valid signin information$/ do
+  fill_in "Email", with: @user.email
+  fill_in "Password", with: @user.password
+  click_button "Sign in"
+end
+
+Then /^she should see her profile page$/ do
+  expect(page).to have_title(@user.name)
+end
+
+And /^she should see a signout link$/ do
+  expect(page).to have_link('Sign out', href: signout_path)
+end
